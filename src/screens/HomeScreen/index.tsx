bda9f333849faa3937a22c83a81754e0d50b25cd
Anıl <anil@Anl-MacBook-Air.local>
2024-02-02 15:11:50 +0300

    location container

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -4,14 +4,18 @@ const index = () => {
   return (
     <View style={styles.headerMain}>
         <View style={styles.headerOne}>
-            <Image />
-            <View>
-                <Text></Text>
-                <Text></Text>
-                <Entypo name="chevron-right" size={20} color="purple" />
+            <Image style={styles.image} source={{uri: "https://cdn.getir.com/misc/emoji/house.png"}}/>
+            <View style={styles.headerOneView}>
+                <Text style={{fontWeight: "600", fontSize: 16}}>Ev</Text>
+                <Text style={{fontWeight: "500", fontSize: 11.5, color: "#6e7480", marginLeft: 6, marginRight: 1}}>Dedepaşa Blv. Yenişehir Mahallesi...</Text>
+                <Entypo name="chevron-right" size={22} color="#5d3ebd" />
             </View>
+            <View style={styles.headerTwo}>
+                <Text style={{fontSize: 10, fontWeight: "bold", color: "#5d3ebd"}}>TVS</Text>
+                <Text style={{fontSize: 20, fontWeight: "bold", color: "#5d3ebd"}}>13dk</Text>
+            </View>
+            
         </View>
-        <View></View>
     </View>
   )
 }
@@ -24,5 +28,34 @@ const styles = StyleSheet.create({
         height: height*0.064,
         backgroundColor: "#f7d102",
     },
-    headerOne: {},
+    headerOne: {
+        height: height* 0.064,
+        width: "80%",
+        backgroundColor: "white",
+        alignItems: "center",
+        flexDirection: "row",
+        paddingHorizontal: "3%",
+        borderTopRightRadius: 25,
+        borderBottomRightRadius: 25,
+    },
+    headerTwo: {
+        width: "25%",
+        backgroundColor: "red",
+        height: height* 0.064,
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    headerOneView: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginLeft: 8,
+        paddingLeft: 8,
+        borderLeftColor: "#f3f2fd",
+        borderLeftWidth: 2,
+    },
+    image: {
+        width: 30,
+        height: 30,
+    },
 });
