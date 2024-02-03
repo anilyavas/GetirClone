6266e459ca8dc5763b7ce3ee7a653567f745fd97
Anıl <anil@Anl-MacBook-Air.local>
2024-02-03 13:35:12 +0300

    categories

--- src/components/CategoryItem/index.tsx
+++ src/components/CategoryItem/index.tsx
@@ -6,8 +6,8 @@ const {width,height} = Dimensions.get("window")
 const CategoryItem = () => {
   return (
     <Pressable>
-        <Image style={{width:width*0.18, height: height: height*0.18  }} ource={}/>
-        <Text></Text>
+        <Image style={{width:width*0.18, height: height*0.18  }} source={{uri: "https://cdn.getir.com/cat/5fd8c58f3bdc2389a56e0fb0_2b1a70ca-4a4a-4477-adf1-7414a476aff8.jpeg"}}/>
+        <Text>İndirimler</Text>
     </Pressable>
   )
 }
